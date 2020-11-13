83f8fe41424a9e13f0a95c05c51825877d1b2600
DzhannaZharaspaeva <61610709+DzhannaZharaspaeva@users.noreply.github.com>
2020-11-13 12:02:36 +0600

    Delete main.kt

--- src/main/kotlin/main.kt
+++
@@ -1,9 +0,0 @@
-import model.Course
-import model.Person
-import model.Task
-import model.Type
-import repo.ListRepo
-
-val persons = ListRepo<Person>()
-val courses = ListRepo<Course>()
-val taskTypes = ListRepo<Type>()
